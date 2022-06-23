7708863bd188abd2748d8c263b01da0f230dc328
Maru Grabelli <86084587+marugrabelli@users.noreply.github.com>
2022-06-23 15:49:35 -0300

    subiendo front end de ecommerce

---
+++ js/footer.js
@@ -0,0 +1,47 @@
+const createFooter=()=>{
+    let footer=document.querySelector("footer");
+    footer.innerHTML=` <div class="footer-content">
+    <img src="img/light-logo.png" class="logo" alt="">
+    <div class="footer-ul-container">
+        <ul class="category">
+            <li class="category-title">men</li>
+            <li><a href="" class="footer-link"> t-shirts</a></li>
+            <li><a href="" class="footer-link"> shoes</a></li>
+            <li><a href="" class="footer-link"> trousers</a></li>
+            <li><a href="" class="footer-link"> skirts</a></li>
+            <li><a href="" class="footer-link"> accessories</a></li>
+            <li><a href="" class="footer-link"> dress</a></li>
+        </ul>
+        <ul class="category">
+            <li class="category-title">men</li>
+            <li><a href="" class="footer-link"> t-shirts</a></li>
+            <li><a href="" class="footer-link"> shoes</a></li>
+            <li><a href="" class="footer-link"> trousers</a></li>
+            <li><a href="" class="footer-link"> skirts</a></li>
+            <li><a href="" class="footer-link"> accessories</a></li>
+            <li><a href="" class="footer-link"> dress</a></li>
+        </ul>
+        
+    </div>
+</div>
+<p class="footer-title">Nosotros</p>
+        <p class="info">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id, ducimus odit reprehenderit fugiat facilis amet sapiente
+             porro similique nostrum animi voluptas harum corrupti quia quaerat eaque, perferendis totam sequi aspernatur!</p>
+<p class="info">info@cloting.com.ar</p>
+<p class="info"> Tel 549+ 11 89564571</p>
+<div class="footer-social-container">
+    <div>
+        <a href="" class="social-link">Tèrminos y Condiciones</a>
+        <a href="" class="social-link"> Polìtica de privacidad</a>
+    </div>
+    <div>
+        <a href="" class="social-link">Instagram</a>
+        <a href="" class="social-link"> Facebook</a>
+    </div>
+    <p class="footer-credit"> Clothing- Todos los derechos reservados-</p>
+
+</div>
+    
+    ;`
+}
+createFooter();
